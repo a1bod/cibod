f0f78f1145b2b23fa2a89b2fc422a41a04af8c14
a1bod <141325010+a1bod@users.noreply.github.com>
2023-09-24 18:08:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Config:
-    API_ID = 7715737 #خلي مثل ماهوه
-    API_HASH = "241e23bd490bfad03f0da62d4f5d8a20" #خلي مثل ماهوه
-    TOKEN = "59758258166:AAEuANb_008uAgVJwQWeDKUYlK5qBMuN8MMT0"  #توكن
-    START_PIC = "https://graph.org/file/9ffcff556e24e0c37e444.jpg" #هنا حط رابط صورة تلكراف
-    CHAT = "-1001852789435" #وهنا خلي ايدي كروب التخزين 
+    API_ID = 7715737 
+    API_HASH = "241e23bd490bfad03f0da62d4f5d8a20" 
+    TOKEN = "6095625065:AAHobldI5Js4Cf3Ws9zhvgCA9NmieRYlpIM" 
+    START_PIC = "https://telegra.ph/file/f146618e52922ba911f03.jpg" #هنا حط رابط صورة تلكراف
+    CHAT = "-1001849313628" #وهنا خلي ايدي كروب التخزين 
 
 
 
