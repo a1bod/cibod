000b501c4e30c8747c9426c4b324d1888688445b
a1bod <141325010+a1bod@users.noreply.github.com>
2023-09-24 18:09:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     API_HASH = "241e23bd490bfad03f0da62d4f5d8a20" 
     TOKEN = "6095625065:AAHobldI5Js4Cf3Ws9zhvgCA9NmieRYlpIM" 
     START_PIC = "https://telegra.ph/file/f146618e52922ba911f03.jpg" #هنا حط رابط صورة تلكراف
-    CHAT = "-1001849313628" #وهنا خلي ايدي كروب التخزين 
+    CHAT = "-1001849313628" 
 
 
 
